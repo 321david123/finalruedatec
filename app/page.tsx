e70e156ad542baaf90004afead881b23cdc34b4e
David Martinez Rodríguez <321davidmr123@gmail.com>
2024-07-09 21:33:09 -0500

    firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import Component from "./component";
 import Chatbot from "./chat/page"
-
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCf2isIZm03rkEa_2W9xk-GzgmqyNrJl-o",
+  authDomain: "ruedatec-9a808.firebaseapp.com",
+  projectId: "ruedatec-9a808",
+  storageBucket: "ruedatec-9a808.appspot.com",
+  messagingSenderId: "193131640842",
+  appId: "1:193131640842:web:b1d8be9e4c794f8e3207f3",
+  measurementId: "G-E9P71GREY7"
+};
+const app = initializeApp(firebaseConfig);
 export default function Home() {
   return (
     <><Component></Component></>
