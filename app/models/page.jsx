9fb5555585dcccde42540a7a4ca8b2b53549ff6c
David Martinez Rodríguez <321davidmr123@gmail.com>
2024-07-17 11:06:58 -0500

    new

--- app/models/page.jsx
+++ app/models/page.jsx
@@ -24,7 +24,7 @@ export default function Models() {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"/>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/inter-ui/3.19.3/inter.css"/>
             <link rel="stylesheet" href="https://321david123.github.io/images/style.css"/>  
-            <button onClick={()=> signOut(auth)}>Cerrar sesión</button>
+            <button onClick={()=> [signOut(auth),alert('se ha cerrado sesion')]}>Cerrar sesión</button>
             <div className="grid">
                 <div className="card">
                     <a href="#" onClick={(e) => handleLinkClick(e, 'https://buy.stripe.com/test_4gwcNCcN20DmguY6oo')}>
